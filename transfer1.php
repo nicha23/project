f181a7d5f71b502f5debe5f01c736175086393b6
sitthisak55110 <sitthisak_u_nick@hotmail.com>
2018-05-27 23:20:22 +0700

    update transfer

--- transfer1.php
+++ transfer1.php
@@ -21,29 +21,79 @@ $Note = mysqli_real_escape_string($con, $_POST['Note']);
 
 
 
-
-if ($BankID=="000") {
+////same bank
+if ($BankID=="000") { //1check bankID
 	
-	//same bank
-	//transaction Sender
-	$sql_send1="INSERT INTO transaction (AccountNo,BankID,ReceiveAccountNo,Amount,Note,transactionCode)
-	VALUES ('$AccountNo','$BankID','$ReceiveAccountNo','$Amount','$Note','A001')";
+	//2query balance sender
+	$sql_balance_send1="SELECT Balance FROM account WHERE AccountNo='$AccountNo'";
+	$query_balance_send1 = mysqli_query($con, $sql_balance_send1);
+	$Balance1=$query_balance_send1['Balance'];
+	//3calculate current balance sender
+	$CurrentBalance1=$Balance1-$Amount;
 
-	//transaction Receiver
-	$sql_rec1="INSERT INTO transaction (AccountNo,BankID,SenderAccountNo,Amount,Note,transactionCode)
-	VALUES ('$ReceiveAccountNo','$BankID','$AccountNo','$Amount','$Note','A003')";
+	//2query balance receiver
+	$sql_balance_send2="SELECT Balance FROM account WHERE AccountNo='$ReceiveAccountNo'";
+	$query_balance_send2 = mysqli_query($con, $sql_balance_send2);
+	$Balance2=$query_balance_send2['Balance'];
+	//3calculate current balance receiver
+	$CurrentBalance2=$Balance2+$Amount;
 
-	//update sender
-	$sql_up_send1="UPDATE account SET Balance=Balance-'$Amount' WHERE AccountNo='$AccountNo'";
+	//4transaction Sender
+	$sql_send1="INSERT INTO transaction (AccountNo,BankID,ReceiveAccountNo,Amount,Total,CurrentBalance,Note,transactionCode)
+	VALUES ('$AccountNo','$BankID','$ReceiveAccountNo','$Amount','$Amount','$CurrentBalance1','$Note','A001')";
+	if (!mysqli_query($con,$sql_send1)) {
+                die('Error: ' . mysqli_error($con));
+            }
 
-	//update receiver
-	$sql_up_rec1="UPDATE account SET Balance=Balance+'$Amount' WHERE AccountNo='$ReceiveAccountNo'";
+	//4transaction Receiver
+	$sql_rec1="INSERT INTO transaction (AccountNo,BankID,SenderAccountNo,Amount,Total,CurrentBalance,Note,transactionCode)
+	VALUES ('$ReceiveAccountNo','$BankID','$AccountNo','$Amount','$Amount','$CurrentBalance2','$Note','A003')";
+	if (!mysqli_query($con,$sql_rec1)) {
+                die('Error: ' . mysqli_error($con));
+            }
+
+	//5update sender
+	$sql_up_send1="UPDATE account SET Balance='$CurrentBalance1' WHERE AccountNo='$AccountNo'";
+	if (!mysqli_query($con,$sql_up_send1)) {
+                die('Error: ' . mysqli_error($con));
+            }
+
+	//5update receiver
+	$sql_up_rec1="UPDATE account SET Balance='$CurrentBalance2' WHERE AccountNo='$ReceiveAccountNo'";
+	if (!mysqli_query($con,$sql_up_rec1)) {
+                die('Error: ' . mysqli_error($con));
+            }
 }
+////other banks
 else {
-	//other banks
-	//transaction Sender
-	$sql_send2="INSERT INTO transaction (AccountNo,BankID,ReceiveAccountNo,Amount,Note,transactionCode)
-	VALUES ('$AccountNo','$BankID','$ReceiveAccountNo','$Amount','$Note','A001')";
+	//2query fee
+	$sql_fee_send2="SELECT fee FROM bank WHERE bankID='$bankID'";
+	$query_fee_send2 = mysqli_query($con, $sql_fee_send2);
+	$fee=$query_fee_send2['fee'];
+	
+	//3calculate total
+	$total=$Amount+$fee;
+
+	//4query balance
+	$sql_balance_send2="SELECT Balance FROM account WHERE AccountNo='$AccountNo'";
+	$query_balance_send2 = mysqli_query($con, $sql_balance_send2);
+	$Balance3=$query_balance_send2['Balance'];
+
+	//5calculate current balance
+	$CurrentBalance3=$Balance3-$Amount;
+
+	//6transaction
+	$sql_send2="INSERT INTO transaction (AccountNo,BankID,ReceiveAccountNo,Amount,Total,CurrentBalance,Note,transactionCode)
+	VALUES ('$AccountNo','$BankID','$ReceiveAccountNo','$Amount','$total','$CurrentBalance3','$Note','A001')";
+	if (!mysqli_query($con,$sql_send2)) {
+                die('Error: ' . mysqli_error($con));
+            }
+
+	//7update sender
+	$sql_up_send1="UPDATE account SET Balance='$CurrentBalance3' WHERE AccountNo='$AccountNo'";
+	if (!mysqli_query($con,$sql_up_send1)) {
+                die('Error: ' . mysqli_error($con));
+            }
 }
 
 
