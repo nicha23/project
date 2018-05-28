8ab99c753e2bfa8bee905e144f9a4a13c039bf86
sitthisak55110 <sitthisak_u_nick@hotmail.com>
2018-05-28 10:37:13 +0700

    update analay
    hahah

--- specialBAR8.php
+++ specialBAR8.php
@@ -1,34 +1,49 @@
 <!DOCTYPE html>
 <html>
-
-	<head>
-		<title>ChartJS - BarGraph</title>
-		<style type="text/css">
-			#chart-container {
-				width: 840px;
-                height: auto;
-			}
-		</style>
-	</head>
-	<body>
-		<center>
-		<div class="col-md-12 ">
-			<div class="title-block">
-					<h2>Count user </h2>
-					<p class="title-description">Count user of ResidentialStatus
-			</div>
-		<div id="chart-container">
-			<canvas id="mycanvas"></canvas>
-		</div>
-
-		<!-- javascript -->
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/Chart.min.js"></script>
-		<script type="text/javascript" src="js/app2.js"></script>
-			</div>
-	</body>
-
-
-
-</center>
+<head>
+ <title>Table with database</title>
+ <style>
+  table {
+   border-collapse: collapse;
+   width: 100%;
+   color: #588c7e;
+   font-family: monospace;
+   font-size: 25px;
+   text-align: left;
+     } 
+  th {
+   background-color: #588c7e;
+   color: white;
+    }
+  tr:nth-child(even) {background-color: #f2f2f2}
+ </style>
+</head>
+<body>
+ <table>
+ <tr>
+  <th>Datetime</th> 
+  <th>Total</th> 
+  <th>Currentbalance</th>
+  <th>Transactioncode</th>
+ </tr>
+ <?php
+$conn = mysqli_connect("localhost", "root", "", "project");
+  // Check connection
+  if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+  } 
+  $sql="SELECT datetime, total, currentbalance, transactioncode FROM transaction WHERE accountNo='50500000003';";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+	echo "<tr><td>" . $row["datetime"]. "</td><td>" . $row["total"] . "</td><td>". $row["currentbalance"]. "</td><td>" 
+	. $row["transactioncode"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
 </html>
